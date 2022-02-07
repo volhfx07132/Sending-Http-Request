0b62092d3dcea3bdbd4529bade07081d671ebc43
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-07 20:31:04 +0700

    Sending a POST Request

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,6 +4,7 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
+  console.log("Data");
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -13,7 +14,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        "https://react-http-9517f-default-rtdb.firebaseio.com/"
+        "https://react-http-9517f-default-rtdb.firebaseio.com/movies.json"
       );
       if (!response.ok) {
         throw new Error("Something went wrong!");
@@ -21,15 +22,18 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for(const key in data){
+        loadedMovies.push({ 
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -54,8 +58,16 @@ function App() {
     content = <p>Loading...</p>;
   }
 
-  const addMovieHandler = (movie) => {
-    console.log(movie);
+  async function addMovieHandler (movie) {
+    const response = await fetch("https://react-http-9517f-default-rtdb.firebaseio.com/movies.json", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': "application/json"
+      }
+    });
+    const data = await response.json();
+    console.log(data);
   };
 
   return (
